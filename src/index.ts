523ddea3455bfa097f1265a6610a3dc226ca2717
Mohit <mohit.peak@gmail.com>
2024-06-01 16:18:51 +0530

    Project setup

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response, Application } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+const app: Application = express();
+
+app.use(cors());
+app.use(express.static("dist"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+import CONFIG from "./config/server_config";
+import apiRouter from "./routes";
+import { isLoggedIn } from "./middlewares/auth_middleware";
+
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", isLoggedIn, (req: Request, res: Response) => {
+  console.log(req.user);
+  res.send("Hello World!");
+});
+
+app.use("/api", apiRouter);
+
+app.listen(CONFIG.PORT, async () => {
+  console.log(`Server started on port ${CONFIG.PORT}`);
+});
